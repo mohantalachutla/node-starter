926d33ec817e1a16609957bc3cd44fdb56c147fb
mohantalachutla <mohan.talachutla@gmail.com>
2022-08-29 11:59:54 +0530

    Update wepack.config: env issue

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,17 @@
 const { merge } = require('webpack-merge')
 
 module.exports = (env, args) => {
-  const environment = args.mode | env.NODE_ENV
+  const environment = args.mode || env.NODE_ENV
   const common = require('./webpack.common.js')
 
+  console.debug({ environment })
   switch (environment) {
-    case 'development' | 'dev':
+    case 'development':
+    case 'dev':
       const dev = require('./webpack.dev.js')
       return merge(common, dev)
-    case 'production' | 'prod':
+    case 'production':
+    case 'prod':
       const prod = require('./webpack.prod.js')
       return merge(common, prod)
     default:
